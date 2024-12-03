c5f5e050d32cb550dfcbd5af3724aef2a1284b61
Cefd2332 <carlos.flores@utec.edu.pe>
2024-12-03 18:16:33 -0500

    fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, ChangeEvent } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import jsPDF from 'jspdf';
